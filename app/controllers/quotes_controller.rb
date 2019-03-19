e823b7079b1168dfdfc5e6e167bf0ac6c5c7e390
Monica Giambitto <m.giambitto@freegoweb.it>
2019-03-19 16:01:34 +0100

    tryexception raising

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,8 @@ class QuotesController < Rulers::Controller
   def a_quote
     "There is nothing either good or bad, but thinking makes it so"
   end
+
+  def exception
+    raise "You Shall Not Pass"
+  end
 end

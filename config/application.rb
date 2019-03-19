296ee5f41c6e2959e0d06a0304cc960f52d42fcc
Monica Giambitto <m.giambitto@freegoweb.it>
2019-03-19 12:52:35 +0100

    require controller files

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,10 @@
 require "rulers"
 
+# No autoloading, adding controller files
+$LOAD_PATH << File.join(File.dirname(__FILE__), "..", "app", "controllers")
+
+require "quotes_controller"
+
 module BestQuotes
   class Application < Rulers::Application
   end
